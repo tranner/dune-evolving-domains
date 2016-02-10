c368fd9528177d8f6f20192dd3b6c71f41fe31b5
Tom Ranner <T.Ranner@leeds.ac.uk>
2016-02-10 13:03:17 +0000

    use errorplot.py to read single domain problem also

--- src/output/errorplot.py
+++ src/output/errorplot.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
 l2OmegaOld = -1
 l2OmegaOld = -1
@@ -12,35 +13,44 @@ hOld = -1
 tauOld = -1
 
 def eoc( a, b ):
+    if b == 0:
+        return -1
     return np.log( a / b )
 
-for i in range( 5 ):
-    try:
-        filename = 'poisson--{0}-.txt'.format( i )
-        my_data = np.genfromtxt( filename , delimiter='  ')
+files = [f for f in os.listdir('.') if os.path.isfile(f) and '.txt' in f]
 
-        h = -1
-        tau = -1
-        file = open( filename )
-        lines = file.readlines()
-        for line in lines:
-            if '# h:' in line:
-                data = line.split()
-                h = float( data[-1] )
-            if '# tau:' in line:
-                data = line.split()
-                tau = float( data[-1] )
-        file.close()
-    except:
-        print 'unable to open', filename
-        continue
+for i,filename in enumerate(files):
+    # try:
+    my_data = np.genfromtxt( filename , delimiter='  ')
+
+    h = -1
+    tau = -1
+    file = open( filename )
+    lines = file.readlines()
+    for line in lines:
+        if '# h:' in line:
+            data = line.split()
+            h = float( data[-1] )
+        if '# tau:' in line:
+            data = line.split()
+            tau = float( data[-1] )
+    file.close()
+    # except:
+    #     print 'unable to open', filename
+    #     continue
 
     # extract data
+    nFields = len(my_data[0])
+
     time    = [ d[0] for d in my_data ]
     l2Omega = [ d[1] for d in my_data ]
     h1Omega = [ d[2] for d in my_data ]
-    l2Gamma = [ d[3] for d in my_data ]
-    h1Gamma = [ d[4] for d in my_data ]
+    if nFields == 5:
+        l2Gamma = [ d[3] for d in my_data ]
+        h1Gamma = [ d[4] for d in my_data ]
+    else:
+        l2Gamma = [ 0 for _ in l2Omega ]
+        h1Gamma = [ 0 for _ in h1Omega ]
 
     # plot
     plt.plot( time, l2Omega, 'b.-' )
@@ -62,6 +72,9 @@ for i in range( 5 ):
     else:
         print '{0:7.4e} {1:7.4e} {2:7.4e} {3} {4} {5:7.4e} {6} {7}'.format( h, tau, l2Omega[-1], '  ---  ', '  ---  ', \
                                                                     l2Gamma[-1], '  ---  ', '  ---  ' )
+
+    if h < 0:
+        print 'read to time ', time[-1]
 
     l2OmegaOld = l2Omega[-1]
     l2GammaOld = l2Gamma[-1]
