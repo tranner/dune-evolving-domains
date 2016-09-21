2cb05ac5aaad27ccfa690854dd2e77437c28ab20
Tom Ranner <T.Ranner@leeds.ac.uk>
2016-09-21 15:05:48 +0100

    add arc details to run script

--- src/source/run.sh
+++ src/source/run.sh
@@ -1,7 +1,14 @@
+# Example script: sample_script.sh
 #!/bin/bash
-
-mkdir -p ../output/surface-3/
-screen -dm ./main-surface-3 fem.prefix:../output/surface-3/ > ../output/surface-3/main.out
+# Set current working directory
+#$ -cwd
+#Use current environment variables/ modules
+#$ -V
+#Request one hour of runtime
+#$ -l h_rt=48:00:00
+#Email at the beginning and end of the job
+#$ -m be
+#Run the executable 'myprogram' from the current working directory
 
 mkdir -p ../output/coupled-2/
-screen -dm ./main-coupled-2 fem.prefix:../output/coupled-2/ > ../output/coupled-2/main.out
+./main-coupled-2 fem.prefix:../output/coupled-2/ > ../output/coupled-2/main.out
